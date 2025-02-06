538de8b804f46c82982d5f93f1b856efdff30475
Kyle Niemeyer <kyleniemeyer@fastmail.com>
2025-02-05 16:54:18 -0800

    adds documentation

---
+++ simple-packaging/docs/source/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'simple-packaging'
+copyright = '2025, Kyle Niemeyer'
+author = 'Kyle Niemeyer'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.intersphinx']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+napoleon_numpy_docstring = True
+
+intersphinx_mapping = {
+  'python': ('https://docs.python.org/3.11', None),
+  'pandas': ('http://pandas.pydata.org/pandas-docs/stable/', None),
+  'numpy': ('https://docs.scipy.org/doc/numpy/', None),
+}
+
+autodoc_default_options = {'members': True}
+autoclass_content = 'class'
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ simple-packaging/docs/source/example.rst
@@ -0,0 +1,5 @@
+=======
+example
+=======
+
+.. automodule:: rosen.example
